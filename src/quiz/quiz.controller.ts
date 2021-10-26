2929791bac38f9a6ed29b619a24a50009b553317
dev.eunjilee <dev.eunji.lee@gmail.com>
2021-10-27 00:00:08 +0900

    tailwind 셋팅\

--- src/quiz/quiz.controller.ts
+++ src/quiz/quiz.controller.ts
@@ -2,10 +2,11 @@ import { Controller, Get, Param, Render } from '@nestjs/common';
 
 @Controller('quiz')
 export class QuizController {
-  @Get(':quizNumber')
-  @Render('viewQuiz')
+  @Get(':quizNumber') // * quizNumber: 파라미터
+  @Render('viewQuiz') // * 해당 파일 이름의 pug 파일을 렌더링
   viewQuiz(@Param('quizNumber') quizNumber: string) {
     const returnObj = {
+      // * 전달 객체
       message: 'Hello world',
       quizNumber: quizNumber,
     };
