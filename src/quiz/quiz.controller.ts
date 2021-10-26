7e7f1642ce8274f1378917e121e99a06d75b8bf6
dev.eunjilee <dev.eunji.lee@gmail.com>
2021-10-26 23:41:03 +0900

    pug 전달 객체

--- src/quiz/quiz.controller.ts
+++ src/quiz/quiz.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Render } from '@nestjs/common';
 
 @Controller('quiz')
 export class QuizController {
   @Get(':quizNumber')
+  @Render('viewQuiz')
   viewQuiz(@Param('quizNumber') quizNumber: string) {
-    console.log('quizNumber');
+    const returnObj = {
+      message: 'Hello world',
+      quizNumber: quizNumber,
+    };
+    return returnObj;
   }
 }
