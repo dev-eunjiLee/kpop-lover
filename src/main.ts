bbf6caa486899f1005c0f6b67566f01dcceb0196
dev.eunjilee <dev.eunji.lee@gmail.com>
2021-10-26 23:28:42 +0900

    템플릿 엔진 및 static 셋팅

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public')); // * public 디렉토리에 정적 자산을 저장
+  app.setBaseViewsDir(join(__dirname, '..', 'views')); // * html 출력 렌더링
+  app.setViewEngine('pug');
+
   await app.listen(3000);
 }
 bootstrap().then();
